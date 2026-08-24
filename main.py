5751cf91d5b7e915264c03e2fa3ffdd51f1249e8
MrMojangnt <jocksandmateovalladares@gmail.com>
2026-08-23 18:05:47 -0600

    ruletitazzz

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def menu():
                     fg="white", bg="steelblue")
     etiqueta.pack(pady=10)
 
-    btnEmpezar = ttk.Button(ventana, text="Empezar", style="Azul.TButton")
+    btnEmpezar = ttk.Button(ventana, text="Empezar", style="Azul.TButton", command = empezar)
     btnEmpezar.pack(pady=10)
 
     btnSalir = ttk.Button(ventana, text="Salir", style="Azul.TButton", command=Salir)
@@ -42,6 +42,39 @@ def menu():
 
     btnCreditos = ttk.Button(ventana, text="Creditos", style="Azul.TButton", command=creditos)
     btnCreditos.pack(pady=10)
+#Empezar
+def empezar():
+    limpiar()
+    lienzo = tk.Canvas(ventana, width=600, height=600, bg= "lightblue")
+    ventana.title("RULETA")
+
+    # colores para la ruleta
+    colores = [
+        "red", "blue", "green", "yellow", "orange", "purple", "cyan", "magenta",
+        "pink", "gold", "coral", "violet", "turquoise", "lime", "salmon", "khaki",
+        "plum", "orchid", "olive", "teal", "navy", "crimson", "tomato", "skyblue"
+    ]
+
+    # nombres de los estudiantes
+    nombres = [
+        "Gretchen Aburto", "Francisco Álvarez", "Guillermo Ayerdis", "Carlos Benavides",
+        "José René Bonilla", "Alex Carballo", "Carlos Castillo", "Raúl Castillo", "Camilo Cruz",
+        "Leah Dávila", "William Hawkins", "Mauricio Lacayo", "Sofía Martínez", "Dorian Martínez",
+        "Reynaldo Mondragón", "Alejandro Mondragón", "Gylbert Ordoñez", "Alyssa Rodríguez", "Shane Rodríguez",
+        "Esmeralda Rodríguez-Salinas", "Francisco Silva", "Evenyer Solorzáno", "Julissa Somarriba",
+        "Miguel Suarez", "Jocksand Valladares"
+    ]
+    contador = 0
+    angulo = -15
+    while contador < 24:
+        angulo = angulo + 15
+        lienzo.create_arc(50, 50, 550, 550, start=angulo, extent=15, fill=colores[contador])
+        contador = contador + 1
+
+    lienzo.grid(column=10, row=0)
+    boton = ttk.Button(ventana, text="Salir", command=ventana.destroy)
+
+    lienzo.create_window(1200, 150, window=boton)
 
 #Creditos
 def creditos():
